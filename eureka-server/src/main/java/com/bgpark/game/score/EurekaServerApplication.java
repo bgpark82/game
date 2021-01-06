0849eca53b7191c21a64c03930e7d6b5e31c29c4
bgpark <bgpark82@gmail.com>
2021-01-06 23:42:05 +0900

    feat: CI/CD 설정

--- eureka-server/src/main/java/com/bgpark/game/score/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/bgpark/game/score/EurekaServerApplication.java
@@ -2,16 +2,16 @@ package com.bgpark.game.score;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.bgpark.game")
-@EnableDiscoveryClient
-public class ScoreApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScoreApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
--- math/src/main/java/com/bgpark/game/math/MathApplication.java
+++ math/src/main/java/com/bgpark/game/math/MathApplication.java
@@ -2,12 +2,14 @@ package com.bgpark.game.math;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.bgpark.game")
+@EnableDiscoveryClient
 public class MathApplication {
 
     @Bean
