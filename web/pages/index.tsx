93489dea48ee41a654ea4861c6f2d24ae7be5ef0
bgpark <bgpark82@gmail.com>
2021-03-01 09:52:17 +0900

    feat: react-toastify 추가

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,10 +2,16 @@ import TodoInputBox from "../components/todos/TodoInputBox";
 import Layout from "../components/commons/Layout";
 import TodosBox from "../components/todos/TodosBox";
 import useTodos from "../hooks/todos/useTodos";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Index() {
 
     const [todo, todos, onChangeTime, onChangeText, onToggleDone, onSubmitTodo] = useTodos();
+    const notify = () => toast("😩 아니, 아직 하고있어??!",{
+        autoClose: false,
+        type:toast.TYPE.DARK
+    });
 
     return (
         <Layout>
@@ -19,6 +25,8 @@ function Index() {
                 todos={todos}
                 onToggleDone={onToggleDone}
             />
+            <button onClick={notify}>notify</button>
+            <ToastContainer/>
         </Layout>
     );
 }
